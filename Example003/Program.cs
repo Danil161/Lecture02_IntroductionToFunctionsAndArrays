ecd3a0a9965dc21d180f08b5ce23d17f7e35c055
Danil <danil.bazarov@inbox.ru>
2023-09-28 18:22:48 +0300

    add Examples003

---
+++ Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int[] array = { 12, 23, 32, 67, 44, 55, 67, 8 };
+             // 0   1   2   3   4   5   6   7
+int n = array.Length;
+int find = 67;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
